a32b5711d7680d8b99b1ed634ef8bb746d34785d
Diana <d7@hotmail.es>
2020-03-15 01:33:41 +0100

    Create component with a double arrow that bounces

---
+++ src/components/ArrowBounce/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import styles from './index.module.scss'
+import DoubleArrowDown from '../../assets/double_arrow_down.svg'
+
+const ArrowBounce = () => <DoubleArrowDown className={styles.bounce} />
+
+export default ArrowBounce
