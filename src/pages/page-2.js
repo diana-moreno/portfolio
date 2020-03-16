a32608e1d5e8276f5f4d2dfb61c447ec8379c10f
Diana <d7@hotmail.es>
2020-03-16 19:34:03 +0100

    style confirmation email page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
+import styles from './styles.module.scss'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+
+    {/* <SEO title="Page two" /> */}
+    <div className={styles.success}>
+      <h1>Thank you!</h1>
+      <div className={styles.title_section_line}></div>
+      <p>Your message has been successfully sent.<br></br> I will contact you very soon!</p>
+      <Link className={styles.goBack} to="/">Go back</Link>
+    </div>
   </Layout>
 )
 
