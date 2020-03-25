a44fe1d9b930f0d040a7ed55a15c6044b759c77f
Diana <d7@hotmail.es>
2020-03-25 22:14:03 +0100

    Add data for concept key

---
+++ src/data/concepts_key.js
@@ -0,0 +1,34 @@
+import ResponsiveIcon from '../assets/responsive.svg'
+import FocusIcon from '../assets/focus.svg'
+// import LightIcon from '../assets/light.svg'
+import CodeIcon from '../assets/code.svg'
+import TendencyIcon from '../assets/tendency.svg'
+
+const concepts_key = [
+  {
+    concept_key: 1,
+    title: 'about.concepts_key.concept_key_1.title',
+    description: "about.concepts_key.concept_key_1.description",
+    icon: ResponsiveIcon
+  },
+  {
+    concept_key: 2,
+    title: 'about.concepts_key.concept_key_2.title',
+    description: "about.concepts_key.concept_key_2.description",
+    icon: FocusIcon
+  },
+  {
+    concept_key: 3,
+    title: 'about.concepts_key.concept_key_3.title',
+    description: "about.concepts_key.concept_key_3.description",
+    icon: CodeIcon
+  },
+  {
+    concept_key: 4,
+    title: 'about.concepts_key.concept_key_4.title',
+    description: "about.concepts_key.concept_key_4.description",
+    icon: TendencyIcon
+  },
+]
+
+export default concepts_key
