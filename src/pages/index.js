fbf8083c6eee3a0dca205ec746d50ca6388969e3
Diana <d7@hotmail.es>
2020-04-04 16:32:41 +0200

    Fix translations of the biografy in spanish

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ import conceptsKey from '../data/concepts_key'
 import { t } from '../i18n'
 import projectsData from '../data/projects.js'
 
-const lang = 'en'
+const lang = 'es'
 
 const IndexPage = ({ pageContext }) => {
   const { image, projects } = useStaticQuery(graphql`
