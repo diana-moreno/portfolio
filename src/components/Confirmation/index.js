c50864a7030938f3e4eb2032e1e13235a3301771
Diana <d7@hotmail.es>
2020-04-11 14:01:17 +0200

    Edit confirmation page and add an animation

--- src/components/Confirmation/index.js
+++ src/components/Confirmation/index.js
@@ -1,19 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link } from 'gatsby'
 import styles from '../../pages/index.module.scss'
-import { t } from '../../i18n';
+import { t } from '../../i18n'
 import allUrls from "../../urls/allUrls"
+import Send from '../../assets/send.svg'
 
-const Confirmation = ({ lang }) => (
-    <div className={styles.success}>
-      <h1>{t('confirmation.title', lang)}</h1>
-      <div className={styles.title_section_line}></div>
-      <p>{t('confirmation.message_1', lang)}<br></br>{t('confirmation.message_2', lang)}</p>
-      <Link 
-        className={styles.goBack} 
-        to={allUrls[1][`url_${lang}`]}
-      >{t('confirmation.button_back', lang)}</Link>
-    </div>
-)
+const Confirmation = ({ lang }) => {
+return (
+  <div className={styles.success}>
+    <Send />
+    <h1>{t('confirmation.title', lang)}</h1>
+    <div className={styles.title_section_line}></div>
+    <p>{t('confirmation.message_1', lang)}<br></br>{t('confirmation.message_2', lang)}</p>
+    <Link 
+      className={styles.goBack} 
+      to={allUrls[1][`url_${lang}`]}
+      >{t('confirmation.button_back', lang)}
+    </Link>
+  </div>
+)}
 
 export default Confirmation
