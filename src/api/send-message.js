597475fd5106395a48be74e2f4487968e2861b19
Diana <d7@hotmail.es>
2020-04-12 23:37:59 +0200

    Delete svg assets not used

--- src/api/send-message.js
+++ src/api/send-message.js
@@ -1,15 +1,8 @@
-// const API_URL = `https://${process.env.API_NAME}.netlify.com` 
-const API_URL = "https://sendmeanemail.herokuapp.com"
-// const API_URL = 'https://sendmeanemail.herokuapp.com'
-
 export default function(name, email, message) {
-  // validate.number(indexBox)
-  // validate.string(description)
-  // validate.string.notVoid('description', description)
+  const API_URL = "https://sendmeanemail.herokuapp.com"
 
   fetch(`${API_URL}/contact`, {
     method: "POST",
-    // mode: "no-cors",
     headers: { "Content-Type": 'application/json',
                 "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type" },
     body: JSON.stringify({ name, email, message })
