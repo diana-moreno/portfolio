a28e70bde063f6df4dd3aa15f1a19828b1ae011e
Diana <d7@hotmail.es>
2020-04-16 18:40:26 +0200

    Add typewritting effect to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,19 +5,29 @@ import ArrowBounce from '../ArrowBounce'
 import { t } from '../../i18n'
 import allUrls from "../../urls/allUrls"
 import PropTypes from "prop-types"
-import Zoom from 'react-reveal/Zoom';
+import Zoom from 'react-reveal/Zoom'
+import Typewriter from 'typewriter-effect';
 
 const Home = ({ lang, homeRef }) => {
   return (
     <section ref={homeRef} id={t('home.ref', lang)} className={styles.home}>
-      <Zoom clear ssrReveal>
-        <div className={styles.greeting}>
-          {t('home.greeting', lang)}<span className={styles.greeting__name}>
-            {t('home.author', lang)}</span>.
+      {/* <Zoom clear ssrReveal> */}
+      <div className={styles.greeting}>
+        {t('home.greeting', lang)}<span className={styles.greeting__name}>
+          {t('home.author', lang)}</span>.
           <br></br>
-          {t('home.author_position', lang)}
-        </div>
-      </Zoom>
+
+          <Typewriter
+            options={{
+              strings: [`${t('home.author_position', lang)}`, `${t('home.creative', lang)}`, `${t('home.design', lang)}`, `${t('home.develop', lang)}`],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 0,
+              delay: 90
+            }}
+          />
+      </div>
+      {/* </Zoom> */}
       <div className={styles.more}>
         <p>{t('home.view_more', lang)}</p>
         <Link to={allUrls[2][`url_${lang}`]}>
