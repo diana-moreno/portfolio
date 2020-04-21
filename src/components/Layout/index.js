420f2ee5fe23125ee012143389eb222275e4d6a1
Diana <d7@hotmail.es>
2020-04-21 19:07:46 +0200

    separate colors in variables

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import "./layout.module.scss"
+import "./index.module.scss"
 import Footer from '../Footer'
 import Seo from "../Seo"
 import 'normalize.css'
